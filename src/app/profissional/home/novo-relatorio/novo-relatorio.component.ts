5b53fab49fa6765ee2481b95fa856cf692def969
Felipe Chernicharo <string7dev@gmail.com>
2021-01-09 15:49:27 -0300

    report form

--- src/app/profissional/home/novo-relatorio/novo-relatorio.component.ts
+++ src/app/profissional/home/novo-relatorio/novo-relatorio.component.ts
@@ -1,5 +1,11 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { NbNativeDateService } from '@nebular/theme';
 import { CalendarFormComponent } from './calendar-form/calendar-form.component';
 import { ReportFormComponent } from './report-form/report-form.component';
@@ -14,21 +20,60 @@ export class NovoRelatorioComponent implements OnInit {
   // date = new Date();
   date: Date = null;
   calendarForm: FormGroup;
-  reportForm;
+  reportForm: FormGroup;
+  consultaForm: FormGroup;
 
-  constructor() {} // @Inject('reportForm') public reportForm: ReportFormComponent // @Inject('calendarForm') public calendarForm: CalendarFormComponent,
+  constructor(private fb: FormBuilder) {} // @Inject('reportForm') public reportForm: ReportFormComponent // @Inject('calendarForm') public calendarForm: CalendarFormComponent,
+
+  get consultas() {
+    return this.reportForm.get('consultas') as FormArray;
+  }
 
   ngOnInit(): void {
     this.calendarForm = new FormGroup({
       date: new FormControl('', Validators.required),
     });
 
-    this.reportForm = new FormArray([]);
+    this.createReportForm();
+    this.addConsulta();
   }
 
   handleDateChange(date: Date) {
-    console.log(date);
     this.date = date;
     this.calendarForm.get('date').setValue(date);
+  }
+
+  createReportForm() {
+    return (this.reportForm = this.fb.group({
+      nomeProfissional: new FormControl(''),
+      idProfissional: new FormControl(''),
+      dataRelatorio: new FormControl(''),
+      dataCriacao: new FormControl(new Date()),
+      dataAtualizacao: new FormControl(new Date()),
+      consultas: this.fb.array([]),
+    }));
+  }
+
+  newConultaForm() {
+    return this.fb.group({
+      nomePaciente: new FormControl('', Validators.required),
+      idPaciente: new FormControl(''),
+      modalidade: new FormControl('', Validators.required),
+      horario: new FormControl('', Validators.required),
+    });
+  }
+
+  getConsultasControls() {
+    return this.consultas.controls;
+  }
+
+  addConsulta() {
+    this.consultas.push(this.newConultaForm());
+  }
+  removeConsulta(i) {
+    console.log('remove ' + i);
+    if (this.consultas.length > 1) {
+      this.consultas.removeAt(i);
+    }
   }
 }
