e9cc2ae30493e0a9244cf474c1e973aa1592c3b9
Felipe Chernicharo <string7dev@gmail.com>
2021-01-10 21:18:37 -0300

    nb-context-menu substituindo nb-menu

--- src/app/structure/header/header.component.ts
+++ src/app/structure/header/header.component.ts
@@ -1,9 +1,13 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import { Router, ActivatedRoute, NavigationStart } from '@angular/router';
 import { appIcons } from '../../../assets/app-icons';
 import { Observable } from 'rxjs';
 import { filter, map, tap } from 'rxjs/operators';
-import { NbMenuItem, NbMenuService } from '@nebular/theme';
+import {
+  NbContextMenuDirective,
+  NbMenuItem,
+  NbMenuService,
+} from '@nebular/theme';
 
 @Component({
   selector: 'app-header',
@@ -12,36 +16,14 @@ import { NbMenuItem, NbMenuService } from '@nebular/theme';
   // encapsulation: ViewEncapsulation.None,
 })
 export class HeaderComponent implements OnInit {
+  @ViewChild(NbContextMenuDirective) contextMenu: NbContextMenuDirective;
+
   defaultUserIcon = appIcons.curlyGuy;
   lacosIcon = appIcons.lacos;
 
   routerState$: Observable<string>;
 
-  menuItems: NbMenuItem[] = [
-    {
-      title: 'Menu',
-      icon: 'menu-outline',
-      expanded: false,
-      children: [
-        {
-          title: 'Novo Relatório',
-          icon: 'plus-outline',
-          link: 'profissional/novo-relatorio',
-        },
-        {
-          title: 'Logout',
-          icon: 'power-outline',
-          link: 'auth',
-        },
-      ],
-    },
-    // {
-    //   title: 'Shopping Bag',
-    // },
-    // {
-    //   title: 'Orders',
-    // },
-  ];
+  menuItems = [];
 
   constructor(
     private router: Router,
@@ -67,6 +49,26 @@ export class HeaderComponent implements OnInit {
         // console.log(state.url.split('/'));
         const currentPage = url.split('/').pop().replace('-', ' ');
         return currentPage;
+      }),
+      tap((page) => {
+        this.menuItems.length = 0;
+        switch (page) {
+          case 'profissional':
+            this.menuItems.push(
+              { title: 'novo-relatorio' },
+              { title: 'logoff' }
+            );
+            break;
+          case 'admin':
+            this.menuItems.push({ title: 'clientes' }, { title: 'logoff' });
+            break;
+          case 'novo relatorio':
+            this.menuItems.push({ title: 'home' }, { title: 'logoff' });
+            break;
+          case 'clientes':
+            this.menuItems.push({ title: 'dashboard' }, { title: 'logoff' });
+            break;
+        }
       })
     );
   }
@@ -88,7 +90,43 @@ export class HeaderComponent implements OnInit {
     }
   }
 
-  onMenuToggle() {
-    this.menuItems[0].expanded = !this.menuItems[0].expanded;
-  }
+  // open() {
+  //   this.contextMenu.show();
+  // }
+
+  // close() {
+  //   this.contextMenu.hide();
+  // }
+
+  // onMenuToggle() {
+  //   this.menuItems[0].expanded = !this.menuItems[0].expanded;
+  // }
 }
+
+// menuItems: NbMenuItem[] = [
+// [
+// {
+// title: 'Menu',
+// icon: 'menu-outline',
+// expanded: false,
+// },
+// children: [
+// {
+// title: 'Novo Relatório',
+// icon: 'plus-outline',
+// link: 'profissional/novo-relatorio',
+// },
+// {
+// title: 'Logout',
+// icon: 'power-outline',
+// link: 'auth',
+// },
+// ],
+// },
+// {
+//   title: 'Shopping Bag',
+// },
+// {
+//   title: 'Orders',
+// },
+// ];
