be2c0c939c095359e91c71cd71a5c87674d30cd6
Felipe Chernicharo <string7dev@gmail.com>
2021-01-20 12:06:49 -0300

    edit-relatorio pt3

--- src/app/profissional/home/edit-relatorio/edit-relatorio.component.ts
+++ src/app/profissional/home/edit-relatorio/edit-relatorio.component.ts
@@ -27,17 +27,17 @@ export class EditRelatorioComponent implements OnInit, OnChanges {
   @Input() reportConsultas: Consulta[];
   editForm: FormGroup;
 
-  reportDate: any;
+  reportDate: string;
   currentConasultas$: any;
 
   constructor(private fb: FormBuilder) {}
 
-  get consultas() {
+  get consultaRows() {
     return this.editForm.get('consultas') as FormArray;
   }
 
   ngOnInit(): void {
-    this.createEditForm();
+    // this.createEditForm();
     // this.newConultaForm();
     // this.reportDate = new Date(this.reportConsultas[0].dataConsulta['seconds']);
     // console.log(this.reportConsultas);
@@ -53,17 +53,48 @@ export class EditRelatorioComponent implements OnInit, OnChanges {
       // console.log(this.reportConsultas);
       this.reportDate = new Date(
         this.reportConsultas[0].dataConsulta['seconds']
-      );
+      ).toLocaleDateString('pt-BR', {
+        weekday: 'long',
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric',
+      });
+
+      this.createEditForm();
+      // this.newConultaForm();
+      this.fillEditForm();
     }
   }
 
   createEditForm() {
     return (this.editForm = this.fb.group({
       dataRelatorio: new FormControl(
-        ''
+        this.reportDate
         // this.reportDate.toLocaleDateString('pt-BR')
       ),
       consultas: this.fb.array([]),
     }));
+  }
+
+  fillEditForm() {
+    this.reportConsultas.forEach((consulta) => {
+      const consultaRow = this.newConultaForm(consulta);
+      this.consultaRows.push(consultaRow);
+    });
+
+    console.log(this.editForm);
+  }
+
+  newConultaForm(consulta: Consulta) {
+    return this.fb.group({
+      nomePaciente: new FormControl(consulta.nomePaciente, Validators.required),
+      // idPaciente: new FormControl(''),
+      // origem: new FormControl(''),
+      modalidade: new FormControl(consulta.modalidade, Validators.required),
+      horario: new FormControl(consulta.horario, [
+        Validators.required,
+        RxwebValidators.unique(),
+      ]),
+    });
   }
 }
